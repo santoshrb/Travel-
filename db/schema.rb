e0a9987b7829ace026553ababb72a6a3b5100527
admin <design@indibasolutions.com>
2018-06-29 16:17:51 +0530

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,122 +11,133 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_101111) do
+ActiveRecord::Schema.define(version: 20180628101111) do
 
-  create_table "branches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "code"
-    t.string "name"
-    t.text "address"
-    t.string "email"
-    t.string "contact_no"
-    t.boolean "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "company_id"
-    t.index ["company_id"], name: "index_branches_on_company_id"
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "code"
-    t.string "name"
-    t.text "address"
-    t.string "country"
-    t.string "state"
-    t.string "district"
-    t.string "city"
-    t.integer "pin_code"
-    t.string "email"
-    t.string "contact_no"
-    t.boolean "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "branches", force: :cascade do |t|
+    t.string   "code",       limit: 255
+    t.string   "name",       limit: 255
+    t.text     "address",    limit: 65535
+    t.string   "email",      limit: 255
+    t.string   "contact_no", limit: 255
+    t.boolean  "status"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "company_id", limit: 8
   end
 
-  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "code"
-    t.string "prefix"
-    t.string "first_name"
-    t.string "middle_name"
-    t.string "last_name"
-    t.date "date_of_birth"
-    t.string "gender"
-    t.string "contact_no"
-    t.string "email"
-    t.string "blood_group"
-    t.string "address"
-    t.string "country"
-    t.string "state"
-    t.string "district"
-    t.string "city"
-    t.integer "pin_code"
-    t.string "adhar_no"
-    t.boolean "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "company_id"
-    t.bigint "branch_id"
-    t.index ["branch_id"], name: "index_employees_on_branch_id"
-    t.index ["company_id"], name: "index_employees_on_company_id"
+  add_index "branches", ["company_id"], name: "index_branches_on_company_id", using: :btree
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "code",       limit: 255
+    t.string   "name",       limit: 255
+    t.text     "address",    limit: 65535
+    t.string   "country",    limit: 255
+    t.string   "state",      limit: 255
+    t.string   "district",   limit: 255
+    t.string   "city",       limit: 255
+    t.integer  "pin_code",   limit: 4
+    t.string   "email",      limit: 255
+    t.string   "contact_no", limit: 255
+    t.boolean  "status"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "enquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "mobile_no"
-    t.string "name_first"
-    t.string "middle_name"
-    t.string "last_name"
-    t.string "email"
-    t.text "address"
-    t.string "place"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "vehicle_type_id"
-    t.index ["user_id"], name: "index_enquiries_on_user_id"
-    t.index ["vehicle_type_id"], name: "index_enquiries_on_vehicle_type_id"
+  create_table "employees", force: :cascade do |t|
+    t.string   "code",          limit: 255
+    t.string   "prefix",        limit: 255
+    t.string   "first_name",    limit: 255
+    t.string   "middle_name",   limit: 255
+    t.string   "last_name",     limit: 255
+    t.date     "date_of_birth"
+    t.string   "gender",        limit: 255
+    t.string   "contact_no",    limit: 255
+    t.string   "email",         limit: 255
+    t.string   "blood_group",   limit: 255
+    t.string   "address",       limit: 255
+    t.string   "country",       limit: 255
+    t.string   "state",         limit: 255
+    t.string   "district",      limit: 255
+    t.string   "city",          limit: 255
+    t.integer  "pin_code",      limit: 4
+    t.string   "adhar_no",      limit: 255
+    t.boolean  "status"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "company_id",    limit: 8
+    t.integer  "branch_id",     limit: 8
   end
 
-  create_table "schemes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.decimal "budget", precision: 10
-    t.string "down_payment"
-    t.decimal "intrest", precision: 10
-    t.date "from_date"
-    t.date "to_date"
-    t.boolean "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.decimal "installment_amount", precision: 10
-    t.string "scheme_type"
-    t.string "installment"
-    t.bigint "vehicle_type_id"
-    t.index ["vehicle_type_id"], name: "index_schemes_on_vehicle_type_id"
+  add_index "employees", ["branch_id"], name: "index_employees_on_branch_id", using: :btree
+  add_index "employees", ["company_id"], name: "index_employees_on_company_id", using: :btree
+
+  create_table "enquiries", force: :cascade do |t|
+    t.string   "mobile_no",       limit: 255
+    t.string   "name_first",      limit: 255
+    t.string   "middle_name",     limit: 255
+    t.string   "last_name",       limit: 255
+    t.string   "email",           limit: 255
+    t.text     "address",         limit: 65535
+    t.string   "place",           limit: 255
+    t.integer  "user_id",         limit: 8
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.integer  "vehicle_type_id", limit: 4
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+  add_index "enquiries", ["user_id"], name: "index_enquiries_on_user_id", using: :btree
+  add_index "enquiries", ["vehicle_type_id"], name: "fk_rails_c8404dfc83", using: :btree
+
+  create_table "schemes", force: :cascade do |t|
+    t.string   "name",               limit: 255
+    t.decimal  "budget",                         precision: 10
+    t.string   "down_payment",       limit: 255
+    t.decimal  "intrest",                        precision: 10
+    t.date     "from_date"
+    t.date     "to_date"
+    t.boolean  "status"
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.decimal  "installment_amount",             precision: 10
+    t.string   "scheme_type",        limit: 255
+    t.string   "installment",        limit: 255
+    t.integer  "vehicle_type_id",    limit: 4
+  end
+
+  add_index "schemes", ["vehicle_type_id"], name: "fk_rails_8115123ef3", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "role",                   limit: 255
   end
 
-  create_table "vehicle_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "code"
-    t.string "name"
-    t.text "description"
-    t.boolean "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "vehicle_types", force: :cascade do |t|
+    t.string   "code",        limit: 255
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.boolean  "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_foreign_key "branches", "companies"
