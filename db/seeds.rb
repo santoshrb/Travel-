66f1168c724de749a12c7575dd923e16a8aefd06
vinod <design@indibasolutions.com>
2018-06-29 17:13:25 +0530

    new changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: "admin@gmail.com", password: "12345678", role: "Admin")
+Company.create(code: "", name: string, address: text, country: string, state: string, district: string, city: string, pin_code: integer, email: string, contact_no: string, status: boolean)
+Branch.create(id: integer, code: string, name: string, address: text, email: string, contact_no: string, status: boolean, company_id: integer)
+Employee.create(id: integer, code: string, prefix: string, first_name: string, middle_name: string, last_name: string, date_of_birth: date, gender: string, contact_no: string, email: string, blood_group: string, address: string, country: string, state: string, district: string, city: string, pin_code: integer, adhar_no: string, status: boolean, company_id: integer, branch_id: integer)
+VehicleType.create(id: integer, code: string, name: string, description: text, status: boolean) 
+Enquiry.create(id: integer, mobile_no: string, name_first: string, middle_name: string, last_name: string, email: string, address: text, place: string, user_id: integer, vehicle_type_id: integer)
+Scheme.create(id: integer, name: string, budget: decimal, down_payment: string, intrest: decimal, from_date: date, to_date: date, status: boolean, installment_amount: decimal, scheme_type: string, installment: string, vehicle_type_id: integer)
