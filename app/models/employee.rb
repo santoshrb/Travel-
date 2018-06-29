c1b948e2491e51280cebe6b37e5a0be1a0f02871
admin <design@indibasolutions.com>
2018-06-29 18:10:25 +0530

    pull

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,6 @@ class Employee < ApplicationRecord
 	validates :last_name, presence:true,uniqueness:{case_sensitive:false}
 	validates :code, presence:true,uniqueness:{case_sensitive:false}
 	validates :email, presence:true,uniqueness:{case_sensitive:false}
-	validates :company_id, presence:true,uniqueness:{case_sensitive:false}
-	validates :branch_id, presence:true,uniqueness:{case_sensitive:false}
+	validates :company_id, presence:true
+	validates :branch_id, presence:true
 end
--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -1,4 +1,7 @@
 class Scheme < ApplicationRecord
 	belongs_to :vehicle_type
 	has_many :enquiries
+	validates :name, presence:true,uniqueness:{case_sensitive:false}
+	validates :scheme_type, presence:true
+	validates :status, presence:true
 end
