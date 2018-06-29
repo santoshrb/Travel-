8f6b2f9d4b7f8f1723c45eb191acb070b5a41feb
dev009 <yo89ku@gmail.com>
2018-06-29 17:47:15 +0530

    pull

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,5 @@
 module CompaniesHelper
+	def all_company
+		Company.all.collect { |x| [x.name, x.id] }
+	end
 end
--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,6 @@
 class Branch < ApplicationRecord
 	# has_many :employees
 	belongs_to :company
+	validates :name, presence:true,uniqueness:{case_sensitive:false}
+	validates :code, presence:true,uniqueness:{case_sensitive:false}
 end
