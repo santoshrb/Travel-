22799edf1b6d79198b57da0e6df13dc1c693855c
dev009 <yo89ku@gmail.com>
2018-07-03 13:15:54 +0530

    pull bigda code

--- app/models/vehicle_booking.rb
+++ app/models/vehicle_booking.rb
@@ -1,2 +1,4 @@
 class VehicleBooking < ActiveRecord::Base
+  validates :pan, presence:true,uniqueness:{case_sensitive:false}
+  validates :adhar, presence:true,uniqueness:{case_sensitive:false}
 end
