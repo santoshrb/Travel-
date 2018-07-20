f64583e8875ee97328694def71f15966c0b69ded
dev009 <yo89ku@gmail.com>
2018-07-20 11:26:20 +0530

    push dashbord and booking selective

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 	def index
-		enq_date = Date.today..(Date.today + 1)
-		@enquiries = Enquiry.where(enquiry_date: enq_date.to_date) 
+		enq_date = (Date.today - 1)..Date.today
+		@enquiries = Enquiry.where(enquiry_date: enq_date) 
 		@schemes = Scheme.all
 		@vehicle_bookings = VehicleBooking.all
 		@companies = Company.all
--- db/migrate/20180719053633_add_reff_to_users.rb
+++ db/migrate/20180719053633_add_reff_to_users.rb
@@ -1,5 +1,5 @@
 class AddReffToUsers < ActiveRecord::Migration
-  # def change
-  #   add_reference :users, :employee, index: true, foreign_key: true
-  # end
+  def change
+    add_reference :users, :employee, index: true, foreign_key: true
+  end
 end
