11e6a5a7bb745c9dd919f6baca0c848d64a3dcef
dev009 <yo89ku@gmail.com>
2018-08-04 15:08:19 +0530

    push

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,10 +3,10 @@ class Employee < ApplicationRecord
 	belongs_to :branch
 	has_many :users
 	has_many :document_lists
-	validates :first_name, presence:true
-	validates :last_name, presence:true
+	validates :first_name, presence:true , length: {minimum: 3}
+	#validates :last_name, presence:true
 	# validates :code, presence:true,uniqueness:{case_sensitive:false}
-	validates :email, presence:true
+	#validates :email, presence:true
 	# validates :company_id, presence:true
 	# validates :branch_id, presence:true
 end
