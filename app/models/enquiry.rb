1b9704929ac46258eeda3d4f5d3b5b43995c7466
Ubuntu <ubuntu@ip-172-31-8-99.ap-southeast-1.compute.internal>
2018-10-01 05:41:49 +0000

    push

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -5,9 +5,12 @@ class Enquiry < ApplicationRecord
   belongs_to :user
 
   validates :name_first, presence:true
-  validates :mobile_no, presence:true,uniqueness:{case_sensitive:false}
+#  validates :mobile_no, presence:true,uniqueness:{case_sensitive:false}
   validates :scheme_id, presence:true
   validates :enquiry_date, presence:true
   
+  validates :mobile_no,  :presence => {:message => 'Mobile should be 10 Number.'},
+                     :numericality => true,
+                     :length => { :minimum => 10, :maximum => 10 }
   #validates :email, presence:true,uniqueness:{case_sensitive:false}
 end
