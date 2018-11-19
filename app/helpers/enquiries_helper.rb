8317ad594094de2c955a70fbb70bde7e6e378222
dev009 <yo89ku@gmail.com>
2018-11-19 14:59:54 +0530

    push

--- app/helpers/enquiries_helper.rb
+++ app/helpers/enquiries_helper.rb
@@ -3,17 +3,17 @@ module EnquiriesHelper
 		if current_user.role.name == "Tellecaller"
 			@booking = VehicleBooking.all.pluck(:enquiry_id)
 			user = User.where(employee_id: current_user.employee_id).take
-			Enquiry.where(user_id: user.id).where.not(id: @booking).collect { |x| [x.mobile_no + ' - ' + x.name_first, x.id] }
+			Enquiry.where(user_id: user.id).where.not(id: @booking).collect { |x| [x.mobile_no + ' - ' + x.name_first+ ' - ' + x.user.employee.first_name, x.id] }
 		elsif current_user.role.name == "GroupAdmin"
 			@booking = VehicleBooking.all.pluck(:enquiry_id)
-			Enquiry.where.not(id: @booking).collect { |x| [x.mobile_no + ' - ' + x.name_first, x.id] }
+			Enquiry.where.not(id: @booking).collect { |x| [x.mobile_no + ' - ' + x.name_first+ ' - ' + x.user.employee.first_name, x.id] }
 		else
 			current_emp = Employee.find_by(id: current_user.employee_id)
 			employee = Employee.where(branch_id: current_emp.branch_id).pluck(:id)
 			user = User.where(employee_id: employee).pluck(:id)
 			@booking = VehicleBooking.all.pluck(:enquiry_id)
-			
-			Enquiry.where.not(id: @booking).where(user_id: user).collect { |x| [x.mobile_no + ' - ' + x.name_first, x.id] }
+
+			Enquiry.where.not(id: @booking).where(user_id: user).collect { |x| [x.mobile_no + ' - ' + x.name_first+ ' - ' + x.user.employee.first_name, x.id] }
 		end
 	end
 end
